7faade9cf6e509795750c68fc030fb941bfb5534
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-12-17 07:11:56 +0100

    Fix the reference to the service worker javascript file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,14 @@
+---
+# Front matter to make Jekyll to post-process it via Liquid
+---
+
 // Only trigger if service workers are supported in browser.
 if ("serviceWorker" in navigator) {
   // Wait until window is loaded before registering.
   window.addEventListener("load", () => {
     // Register the service worker with "/" as it's scope.
     navigator.serviceWorker
-      .register("/scripts/service-worker.js", { scope: "/" })
+      .register({{ "/scripts/service-worker.js" | prepend: site.baseurl | prepend: site.url }}, { scope: "/" })
       // Output success/failure of registration.
       .then(() => console.log("SW registered"))
       .catch(() => console.error("SW registration failed"));
